2261908dd76a04c0276b6793771882a40ad9557a
lovely singh2819 <loviidovii12@gmail.com>
2023-09-01 02:05:18 +0530

    done

---
+++ index.js
@@ -0,0 +1,20 @@
+// const resume_btn1 = document.querySelector("#resume-button-1");
+// const resume_btn2 = document.querySelector("#resume-button-2");
+
+// resume_btn1.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   window.open(
+//     "https://drive.google.com/file/d/1yyGMLlsq9psmV5--n4hYf_rGmK8XFoR5/view?usp=sharing"
+//   );
+//   window.location.href =
+//     "https://drive.google.com/uc?export=download&id=1yyGMLlsq9psmV5--n4hYf_rGmK8XFoR5";
+// });
+
+// resume_btn2.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   window.open(
+//     "https://drive.google.com/file/d/1yyGMLlsq9psmV5--n4hYf_rGmK8XFoR5/view?usp=sharing"
+//   );
+//   window.location.href =
+//   "https://drive.google.com/uc?export=download&id=1yyGMLlsq9psmV5--n4hYf_rGmK8XFoR5";
+// });
